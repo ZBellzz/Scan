2fdfbf59303359039a8f043b82eaef03a8a64f28
ZBellzz <petrusramos1@gmail.com>
2024-05-27 21:25:06 -0300

    commit 1

---
+++ estrutura/JAVA/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider-principal').slick({
+        autoplay: true, // Ativar autoplay
+        autoplaySpeed: 6000, // Definir o intervalo de troca de imagens em milissegundos (6 segundos)
+        infinite: true // Permitir rolagem infinita
+    });
+});
